11c188400bfe78b96b43149ab06b0836518f38a5
Chris Dunn-Birch <chris@dunnbirch.com>
2020-01-17 17:54:41 +0000

    Neater comments.

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,9 @@
-/**
- * This order causes no errors in `yarn start` but causes an error in the story for component A
- */
+// This order causes no errors in `yarn start` but causes an error in the story for component A
 import { A } from "./A";
 import { B } from "./B";
-/**
- * This order causes no errors anywhere
-import { B } from "./B";
-import { A } from "./A";
- */
+// This order causes no errors anywhere
+// import { B } from "./B";
+// import { A } from "./A";
 
-/**
- * It doesn't matter in which order we export (A,B or B,A), only the order of the import causes the error
- */
+// It doesn't matter in which order we export (A,B or B,A), only the order of the import causes the error
 export { A, B };
