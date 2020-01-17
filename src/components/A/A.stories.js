bc5852cfebbcef81ec95d48daca3cb20ed26195c
Chris Dunn-Birch <chris@dunnbirch.com>
2020-01-17 17:15:02 +0000

    Installs Storybook.

---
+++ src/components/A/A.stories.js
@@ -0,0 +1,10 @@
+// React
+import React from "react";
+// Components
+import { A } from "components";
+
+export default {
+  title: "A",
+};
+
+export const Story = () => <A />;
---
+++ src/components/B/B.stories.js
@@ -0,0 +1,10 @@
+// React
+import React from "react";
+// Components
+import { B } from "components";
+
+export default {
+  title: "B",
+};
+
+export const Story = () => <B />;
