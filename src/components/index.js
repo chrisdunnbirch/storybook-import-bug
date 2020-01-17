ef0c4652527401257246f8228ab2fe97f7d20227
Chris Dunn-Birch <chris@dunnbirch.com>
2020-01-17 17:18:57 +0000

    Further explanation in comment.

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,10 @@
 /**
- * This order causes an error in the story for component A
+ * This order causes no errors in `yarn start` but causes an error in the story for component A
  */
 import { A } from "./A";
 import { B } from "./B";
-
 /**
- * This order causes no errors:
-
+ * This order causes no errors anywhere
 import { B } from "./B";
 import { A } from "./A";
  */
