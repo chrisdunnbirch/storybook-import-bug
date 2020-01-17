631b3095e2a1f10fe469535293a3f414b2185a95
Chris Dunn-Birch <chris@dunnbirch.com>
2020-01-17 17:15:48 +0000

    Adds comments for debugging purposes.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,14 @@
+/**
+ * This order causes an error in the story for component A
+ */
 import { A } from "./A";
 import { B } from "./B";
+
+/**
+ * This order causes no errors:
+
+import { B } from "./B";
+import { A } from "./A";
+ */
 
 export { A, B };
